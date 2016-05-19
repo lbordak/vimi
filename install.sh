005874a585ce3396e1de8bf43a3a63c841798d3a
GotBinGo <tamas@webtar.hu>
2016-05-20 00:22:43 +0200

    base

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get install vim git
+echo got vim
+git clone http://github.com/w0ng/vim-hybrid
+mkdir ~/.vim -p
+cp vim-hybrid/colors ~/.vim/ -r
+cp vimrc ~/.vimrc
---
+++ vimrc
@@ -0,0 +1,10 @@
+map <F5> :w<CR>:!make run<CR>
+map <F6> :w<CR>:!make<CR>
+
+set tabstop=4
+set shiftwidth=4
+
+set t_Co=256
+set background=dark
+colorscheme hybrid
+
